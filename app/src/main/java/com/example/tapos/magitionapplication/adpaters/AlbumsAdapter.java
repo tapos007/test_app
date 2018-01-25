dfe55d362a0544538a70a21242d1c5ae249eb3cf
tapos ghosh <tapos.aa@gmail.com>
2018-01-25 21:18:52 +0600

    first day class done

---
+++ app/src/main/java/com/example/tapos/magitionapplication/adpaters/AlbumsAdapter.java
@@ -0,0 +1,61 @@
+package com.example.tapos.magitionapplication.adpaters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.tapos.magitionapplication.R;
+import com.example.tapos.magitionapplication.models.Album;
+
+import java.util.List;
+
+/**
+ * Created by tapos on 1/22/18.
+ */
+public class AlbumsAdapter extends RecyclerView.Adapter<AlbumsAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<Album> albumList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title ;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+        }
+    }
+
+
+    public AlbumsAdapter(Context mContext, List<Album> albumList) {
+        this.mContext = mContext;
+        this.albumList = albumList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.album_card, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        Album album = albumList.get(position);
+        holder.title.setText(album.getName());
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return albumList.size();
+    }
+}
